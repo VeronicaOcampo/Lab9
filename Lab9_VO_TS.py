19c91ac088e8e4cc862020d17ce615904469c79a
VeronicaOcampo <166737424+VeronicaOcampo@users.noreply.github.com>
2024-04-11 12:32:12 -0400

    Add files via upload

---
+++ Lab9_VO_TS.py
@@ -0,0 +1,29 @@
+def encode(password):
+    encoded_pass = ''
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10) #shifts each digit up by 3 numbers
+        encoded_pass += encoded_digit
+    return encoded_pass
+
+def main():
+    while True:
+        print('Menu\n'
+            '-------------\n'
+            '1. Encode\n'
+            '2. Decode\n'
+            '3. Quit\n')
+
+        option = input('Please enter an option: ')
+        if option == '1':
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!\n')
+
+
+
+        if option == '3':
+            break
+
+
+if __name__ == '__main__':
+    main()
